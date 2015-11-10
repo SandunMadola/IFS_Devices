2aab326a4b5f1828b259aaf47ce4609e9ea91699
Sandun Madola <sandunmadola7@gmail.com>
2015-11-10 16:48:21 +0530

    IFS_Devices init

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var app = angular.module('app', ['ngRoute', 'homeCtrls']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+                when('/home', {
+                    templateUrl: 'dynamic/home.html',
+                    controller: 'homeCtrl'
+                }).
+                when('/devices', {
+                    templateUrl: 'dynamic/devices.html',
+                    controller: 'deviceCtrl'
+                }).
+                when('/thumbnail', {
+                    templateUrl: 'dynamic/thumbnail.html',
+                    controller: 'thumbnailCtrl'
+                }).
+                when('/details/:itemId', {
+                    templateUrl: 'dynamic/details.html',
+                    controller: 'detailsCtrl'
+                }).
+                when('/request', {
+                    templateUrl: 'dynamic/request.html',
+                    controller: 'requestCtrl'
+                }).
+                when('/settings', {
+                    templateUrl: 'dynamic/settings.html',
+                    controller: 'settingsCtrl'
+                }).                        
+                otherwise({
+                    redirectTo: '/home'
+                });
+    }]);
+
---
+++ js/controller.js
@@ -0,0 +1,82 @@
+var homeCtrls = angular.module('homeCtrls', []);
+
+homeCtrls.controller('homeCtrl', ['$scope', function ($scope) {
+
+    }]);
+
+homeCtrls.controller('deviceCtrl', ['$scope', '$http', function ($scope, $http) {
+        $http.get('js/data.json').success(function (data) {
+            $scope.devices = data;
+        });
+        $scope.popup = $(document).ready(function () {
+            $('modal1').show();
+        });
+
+        $(document).ready(function () {
+            // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+            $('.modal-trigger').leanModal();
+        });
+
+    }]);
+
+homeCtrls.controller('detailsCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+        $http.get('js/data.json').success(function (data) {
+            $scope.devices = data;
+            $scope.whichItem = $routeParams.itemId;
+        });
+        $scope.mo = true;
+        $scope.popup = function () {
+            if ($scope.mo == false) {
+                $scope.mo = true;
+            } else {
+                $scope.mo = false;
+            }
+        };
+
+        $(document).ready(function () {
+            // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+            $('.modal-trigger').leanModal();
+        });
+
+
+    }]);
+
+homeCtrls.controller('requestCtrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.some = "Request a device";
+        $scope.update = angular.copy.request;
+//        $scope.update = angular.copy.later;
+
+        $(document).ready(function () {
+            $('select').material_select();
+        });
+        $(document).ready(function () {
+            $('input#input_text,input#un,input#un2,input#icon_prefix, textarea#textarea1').characterCounter();
+        });
+        $(document).ready(function () {
+            $('.collapsible').collapsible({
+                accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+            });
+        });
+        $(document).ready(function () {
+            // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+            $('.modal-trigger').leanModal();
+        });
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
+
+    }]);
+homeCtrls.controller('settingsCtrl', ['$scope', '$http', function ($scope, $http) {
+//        $http.get('js/data.json').success(function (data) {
+//            $scope.devices = data;
+//        });
+        $scope.some = "Requsted Devices";
+        $(document).ready(function () {
+            $('ul.tabs').tabs();
+        })
+    }]);
+
+homeCtrls.controller('thumbnailCtrl', ['$scope', function ($scope) {
+        $scope.some = "This is thumbnail page";
+    }]);
